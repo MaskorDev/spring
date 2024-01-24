1ddcd68d8f8aafe9f48512d64ecbc03483e93cb2
TrueXz <maskor.gpt@gmail.com>
2024-01-24 12:28:23 +0500

    Added post and get requests

--- src/main/java/com/TrueXz/spring/book/controller/BookApiController.java
+++ src/main/java/com/TrueXz/spring/book/controller/BookApiController.java
@@ -1,12 +1,35 @@
 package com.TrueXz.spring.book.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.TrueXz.spring.base.exception.ResourceNotFoundException;
+import com.TrueXz.spring.book.entity.BookEntity;
+import com.TrueXz.spring.book.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookApiController {
+    private final BookService bookService;
+
+    public BookApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/")
     public String ok() {
         return "ok";
+    }
+    @GetMapping("/api/v1/book")
+    public List<BookEntity> all() {
+        return bookService.all();
+    }
+    @GetMapping("/api/v1/book/{id}")
+    public BookEntity byId(@PathVariable Integer id) {
+        return bookService.byId(id).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @PostMapping("/api/v1/book")
+    public BookEntity create(@RequestBody BookEntity request) {
+        return bookService.create(request.getTitle(), request.getDescription());
     }
 }
---
+++ src/main/java/com/TrueXz/spring/book/service/BookService.java
@@ -0,0 +1,48 @@
+package com.TrueXz.spring.book.service;
+
+import com.TrueXz.spring.book.entity.BookEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class BookService {
+    static List<BookEntity> bookStorage = new ArrayList<>();
+
+    public BookService() {
+        fillStorage();
+    }
+
+
+    public void fillStorage() {
+        Random random = new Random();
+        for (int i = 0; i <= 100; i++) {
+            BookEntity book = new BookEntity();
+            book.setId(i);
+            book.setTitle("Book #" + random.nextInt(100, 999));
+            book.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
+                    "eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+            bookStorage.add(book);
+        }
+    }
+
+    public List<BookEntity> all() {
+        return bookStorage;
+    }
+
+    public Optional<BookEntity> byId(Integer id) {
+        return bookStorage.stream().filter(book -> book.getId().equals(id)).findFirst();
+    }
+
+    public BookEntity create(String title, String description) {
+        BookEntity book = new BookEntity();
+        book.setId(bookStorage.size());
+        book.setTitle(title);
+        book.setDescription(description);
+        bookStorage.add(book);
+        return book;
+    }
+}
